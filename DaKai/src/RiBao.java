f3363a97131c3573cbc66fcc5d4464047a01899e
Mrmaer <rjyw@LAPTOP-GMECQ4I8>
2017-09-01 17:34:18 +0800

    差视图

--- DaKai/src/RiBao.java
+++ DaKai/src/RiBao.java
@@ -1,14 +1,11 @@
 import java.io.File;
 import java.io.IOException;
 
+import swing.Docke;
+
 public class RiBao {
 	public static void main(String[] args) {
-		try {
-			java.awt.Desktop.getDesktop().open(new File("Â·¾¶"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
+		new Docke().docke();
 	}
 }
 
